5931f163c45a67749e1e1f96e1a3b77e6db47c24
devYuno <annykessy20@gmail.com>
2026-02-03 10:24:07 -0300

    att find

--- findpeople.js
+++ findpeople.js
@@ -1,3 +1,14 @@
 use('bosch')
-
 db.people.find({ name: /^J.*s$/ })
+
+use('bosch')
+db.people.find({ name: 'Cesar', lastname: 'Stati' })
+
+use('bosch')
+db.people.find({ $and: [
+    { name: 'Cesar'},
+    { lastname: 'Lima' }
+]})
+
+use('bosch')
+db.people.find({ salary: { $gte: 99 } }, { name: 1, lastname: 1})
