0f04c13c080c84d94c37b796c1003ef500032b7f
devYuno <annykessy20@gmail.com>
2026-02-09 10:28:08 -0300

    sei la

--- aula-node/src/server.ts
+++ aula-node/src/server.ts
@@ -4,6 +4,38 @@ const port = 8080;
 
 const app = express();
 
+const pessoa = {
+    nome: "Kessyane",
+    sobrenome: "Santos"
+}
+
+const pessoas = {
+    kessyane: {
+        nome: "Kessyane",
+        sobrenome: "Santos" 
+    },
+    joyce: {
+        nome: "Joyce",
+        sobrenome: "Nascimento"
+    },
+    fernanda: {
+        nome: "Fernanda",
+        sobrenome: "Fialho"
+    },
+    thayna: {
+        nome: "Thayna",
+        sobrenome: "Kaizi"
+    }
+}
+
+app.get('/pessoas', (req, res) => {
+    res.status(200).send({ pessoas: pessoas })
+})
+
+app.get('/pessoa', (req, res) => {
+    res.send({ pessoa: pessoa })
+})
+
 app.get('/', (req, res) => {
     res.send({ response: "Api funcionando!"})
 })
