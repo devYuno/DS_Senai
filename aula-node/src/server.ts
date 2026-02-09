5095854d67f1bd1e99dc5df3204f37a9ff14ef4c
devYuno <annykessy20@gmail.com>
2026-02-09 09:21:01 -0300

    FEAT: initial dev

---
+++ aula-node/src/server.ts
@@ -0,0 +1,13 @@
+import express from 'express';
+
+const port = 8080;
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send({ response: "Api funcionando!"})
+})
+
+app.listen(port, () => {
+    console.log(`Server rodando na porta ${port}`)
+})
