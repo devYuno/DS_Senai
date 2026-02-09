d8d0a8915929afac35934ff34ea2afa9de06982d
devYuno <annykessy20@gmail.com>
2026-02-09 11:37:31 -0300

    get e usersss

--- aula-node/src/routes/person.ts
+++ aula-node/src/routes/person.ts
@@ -3,4 +3,15 @@ import express, {Request, Response, Router } from 'express';
 const router: Router = express.Router();
 const people: object[] = [];
 
+router
+    .post('/registrar', (req: Request, res: Response) => {
+        const { nome, sobrenome } = req.body 
+        console.log(nome, sobrenome)
+        people.push({nome, sobrenome})
+        res.status(200).send({ message: `Usuário ${nome} ${sobrenome} cadastrado com sucesso!`})
+    })
+    .get('/usuarios', (req: Request, res: Response) => {
+        res.status(200).send({ users: people })
+    })
+
 export default router;
--- aula-node/src/server.ts
+++ aula-node/src/server.ts
@@ -1,7 +1,14 @@
 import express from 'express';
+import routes from './routes/routes.ts'
 
 const port = 8080;
 const app = express();
+
+routes(app);
+
+app.get('/', (req, res) => {
+    res.status(200).send({response: "Api funcionando!"})
+})
 
 app.listen(port, () => {
     console.log(`Server rodando na porta ${port}`)
