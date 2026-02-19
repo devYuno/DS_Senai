726d62bf1f5e5f3c0c6a1ce2c85e5224f8ec6bbb
devYuno <annykessy20@gmail.com>
2026-02-19 10:57:17 -0300

    salvando

--- aula-node/src/routes/person.ts
+++ aula-node/src/routes/person.ts
@@ -4,6 +4,9 @@ const router: Router = express.Router();
 const people: object[] = [];
 
 router
+   .get('/', (req, res) => {
+        res.status(200).send({response: "person funcionando!"})
+    })
     .post('/registrar', (req: Request, res: Response) => {
         const { nome, sobrenome } = req.body 
         console.log(nome, sobrenome)
@@ -12,6 +15,32 @@ router
     })
     .get('/usuarios', (req: Request, res: Response) => {
         res.status(200).send({ users: people })
+    })
+    .get('/usuarios/:id', (req, res) => {
+        const { id } = req.params
+        // res.status(200).send({ response: id })
+        let convertId = Number(id)
+        res.status(200).send({ response: people[convertId] })
+    })
+    .get('/filtro', (req, res) => {
+        const { nome, sobrenome } = req.query
+        res.status(200).send({ response: `${nome} ${sobrenome}` })
+    })
+
+    .put('/atualizar/:id', (req, res) => {
+        const { id } = req.params
+        const { nome, sobrenome } = req.body
+        res.status(200).send({ response: `Atualizando o usuário ${id} -> ${nome} ${sobrenome}`})
+    })
+    .patch('/atualizar/:id', (req, res) => {
+        const { id } = req.params
+        const { nome } = req.body
+        res.status(200).send({ response: `Atualizando o usuário ${id} -> ${nome}`})
+    })
+
+    .delete('/delete/:id', (req, res) => {
+        const { id } = req.params
+        res.status
     })
 
 export default router;
