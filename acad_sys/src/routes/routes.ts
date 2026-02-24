51c6fc2e0dcd21f79d5016c6e4e333e4a28b4b89
devYuno <annykessy20@gmail.com>
2026-02-24 11:41:07 -0300

    FEAT: get users

--- acad_sys/src/routes/routes.ts
+++ acad_sys/src/routes/routes.ts
@@ -1,5 +1,6 @@
 import { Express } from "express";
 import express from 'express';
+import users from './users.ts'
 
 export default function (app: Express) {
     app
--- acad_sys/src/routes/users.ts
+++ acad_sys/src/routes/users.ts
@@ -11,11 +11,43 @@ interface user {
     createdAt: Date;
 }
 
+const nextId = 0;
+
 const users: user[] = []
 
 router
     .get('/', (req, res) => {
-        if (users === 0) {
-            
+        if (users.length == 0) {
+            res.status(404).send({ response: "No registered users found." })
+        }
+        else {
+            res.status(200).send({ users: users })
         }
     })
+    .get('/:id', (req, res) => {
+        const { id } = req.params
+        let convertId = Number(id)
+        const query = users.find(u => u.id === convertId)
+        if (query) {
+            res.status(200).send({ user: query })
+        }
+        else {
+            res.status(404).send({ response: "User not found!" })
+        }
+    })
+    .post('/register', (req, res) => {
+        const {
+            name,
+            email,
+            type
+        } = req.body
+
+        let id = nextId;
+        let active = true;
+        let createdAt = new Date();
+
+        users.push({ id, name, email, type, active, createdAt })
+        res.status(201).send({ message: "User registered successfully!" })
+    })
+
+export default router;
